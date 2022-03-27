18cf383dbf0a3001898da42a67c9479636bb9a7f
khuatlong0902 <khuatlong0902@gmail.com>
2022-03-27 16:02:08 +0700

    created project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './index.css'
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
